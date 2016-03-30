1779caa104805e4e04c6f18a4e44312fd9e123b2
unknown <aeagr@IT051727.users.bris.ac.uk>
2016-03-30 11:07:06 +0100

    Added new test shapes and result storage
    Modified gitignore to skip result data files
    
    Signed-off-by: unknown <aeagr@IT051727.users.bris.ac.uk>

--- compSequence.m
+++ compSequence.m
@@ -4,12 +4,30 @@ clear all
 % flight parameters
 Rmin = 0.75;
 vAir = 1;
-vWind = [0.4,-0.4];
+vWind = [0.6,-0.1];
 
-% field polygon
 % simple square
-P = [0 10 10 0;
-    0  0 10 10]*0.999;
+Psq = [0 10 10  0;
+       0  0 10 10]*0.999;
+
+% rectangle
+Prect = [0 5  5  0;
+         0 0 20 20]*0.999;
+
+% rectangle, this time without whole numbered sides
+Prect2 = [0 4.5  4.5  0;
+         0   0 22.2 22.2];
+
+% rectangle, this time without whole numbered sides
+% and now with even number of strips
+Prect3 = [0 5.5  5.5  0;
+         0   0 18.2 18.2];
+
+% choose test case
+P = Prect3;
+     
+% find centroid
+pCent = mean(P,2);
 
 % strip width
 wid = 1;
@@ -17,17 +35,16 @@ wid = 1;
 % strip offset
 ofs = -0.0;
 
-% strip angle
-angs = linspace(-pi,pi,17);
+% strip angle range to try
+Nangs = 128;
+angs = (1:Nangs)*2*pi/Nangs;
 
-for kk=1:17,
+for kk=1:numel(angs),
     
     ang = angs(kk);
     
     % divide into strips
     [strips,flights] = stripPoly(P,ang,wid,ofs);
-    
-    figure(kk)
     
     % plot the original field
     patch(P(1,:),P(2,:),'g')
@@ -51,11 +68,22 @@ for kk=1:17,
     [pp,pt,scanTime,turnTime] = simpleOrderedSequence(flights,vAir,Rmin,vWind);
     plot(pp(1,:),pp(2,:),'b-', ...
         pp(1,1),pp(2,1),'b^')
+    plot(pCent(1)+[0 vWind(1)],pCent(2)+[0,vWind(2)],'r-','LineWidth',2)
+    plot(pCent(1),pCent(2),'ro','LineWidth',2)
     
     title(sprintf('%.0f^o : %.1f scanning and %.1f turning',ang*180/pi,scanTime,turnTime))
     
     scanTimeList(kk) = scanTime;
     turnTimeList(kk) = turnTime;
+    
+    % print the figure
+    fname=sprintf('results/rect3_windacross_n%d',kk);
+    save([fname '.mat']);
+    print('-dpng',[fname '.png']);
+    % clear it
+    clf
+    % or open a new one
+    % figure
     
 end
 %%
