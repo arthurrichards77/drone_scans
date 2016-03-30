d7195221008963c14512a314b1ff8f9d8dbe7961
aeagr <arthur.richards@bristol.ac.uk>
2016-03-30 17:13:36 +0100

    Robustness improvements to wind turn calculations
    No longer at the mercy of fzero
    
    Signed-off-by: aeagr <arthur.richards@bristol.ac.uk>

--- shortestWindPath.m
+++ shortestWindPath.m
@@ -2,13 +2,151 @@ function [px,py,pt,pxa,pya,clInc]=shortestWindPath(cInit,cTerm,Rmin,vFly,vWind)
 %
 %
 
-% initialise search
-dStar = inf;
-clStar = [];
+% debug flag - plot G(d) functions
+dbgPlot = false;
+
+% bisection tolerance
+tolBisect = 1e-5;
 
 % start by searching over default path classes
-[d0,G0] = fzero(@(d)targetTimeDiff(d,cInit,cTerm,Rmin,vWind,vFly,[]),0.0);
+% lower bound
+dLB = 0;
+[gLB,pxLB,pyLB,ptLB,clLB] = targetTimeDiff(dLB,cInit,cTerm,Rmin,vWind,vFly,[]);
+% gone wrong if g not zero at zero
+assert(gLB>=0)
 
+if dbgPlot,
+    dgs = [dLB gLB];
+end
+
+% upper bound
+dUB = 1;
+[gUB,pxUB,pyUB,ptUB,clUB] = targetTimeDiff(dUB,cInit,cTerm,Rmin,vWind,vFly,[]);
+
+if dbgPlot,
+    dgs = [dgs; dUB gUB];
+end
+
+% stage 1 - expand until enclosing a zero crossing
+while gUB>0,
+    dLB = dUB;
+    dUB = 2*dUB;
+    [gUB,pxUB,pyUB,ptUB,clUB] = targetTimeDiff(dUB,cInit,cTerm,Rmin,vWind,vFly,[]);
+    if dbgPlot,
+        dgs = [dgs; dUB gUB];
+        dmax = dUB;
+    end
+end
+
+% stage 2 - bisection search
+while dUB>dLB+tolBisect,
+    dT = 0.5*(dUB+dLB);
+    [gT,pxa,pya,pt,clInc] = targetTimeDiff(dT,cInit,cTerm,Rmin,vWind,vFly,[]);
+    if gT<0,
+        dUB = dT;
+        gUB = gT;
+        clUB = clInc;
+    else
+        dLB = dT;
+        gLB = gT;
+        clLB = clInc;
+    end
+    if dbgPlot,
+        dgs = [dgs; dT gT];
+    end
+end
+
+% check for discontinuity - am I riding same line both sides?
+if ~strcmp(clLB,clUB),
+    %disp('Discontinuity')
+    % time to work right from the upper bound
+    dT = dUB;
+    % cautious steps here
+    dStep = dUB/20;
+    % initialize for each case
+    for cc=1:8,
+        [thisG,~,~,~,~] = targetTimeDiff(dT,cInit,cTerm,Rmin,vWind,vFly,((1:8)==cc));
+        if isempty(thisG),
+            gLast(cc)=NaN;
+        else
+            gLast(cc)=thisG;
+        end
+    end
+    % winning class placeholder
+    ccWins = 0;
+    % linear search this time
+    while ccWins==0,
+        dT = dT + dStep;
+        % initialize for each case
+        for cc=1:8,
+            clMask = ((1:8)==cc);
+            [thisG,pxa,pya,pt,clInc] = targetTimeDiff(dT,cInit,cTerm,Rmin,vWind,vFly,clMask);
+            if ~isempty(thisG),
+                if thisG*gLast(cc)<=0,
+                    % sign change - winner!
+                    ccWins = cc;
+                    break
+                else
+                    % store for next time
+                    gLast(cc)=thisG;
+                end
+            end
+            if dbgPlot,
+                dgs = [dgs; dT thisG];
+            end
+        end
+    end
+    % found first sign change - now final bisection search
+    dUB = dT;
+    dLB = dT-dStep;
+    % need to check if rising or falling
+    gUBsign = sign(thisG);
+    while dUB>dLB+tolBisect,
+        dT = 0.5*(dUB+dLB);
+        [gT,pxa,pya,pt,clInc] = targetTimeDiff(dT,cInit,cTerm,Rmin,vWind,vFly,clMask);
+        if gT*gUBsign>0,
+            dUB = dT;
+        else
+            dLB = dT;
+        end
+        if dbgPlot,
+            dgs = [dgs; dT gT];
+        end
+    end
+    
+end
+
+% shift back by the wind
+px = pxa + vWind(1)*pt/vFly;
+py = pya + vWind(2)*pt/vFly;
+
+% optional plotting of G functions for debug
+if dbgPlot,
+    f=gcf;
+    figure
+    plot(dgs(:,1),dgs(:,2),'x')
+    hold on
+    dsRng = linspace(0,2*dmax,100);
+    for cc=1:8,
+        clTest = ((1:8)==cc);
+        for ii=1:numel(dsRng),
+            [thisG,~,~,~,~] = targetTimeDiff(dsRng(ii),cInit,cTerm,Rmin,vWind,vFly,clTest);
+            if ~isempty(thisG),
+                gsRng(ii,cc) = thisG;
+            else
+                gsRng(ii,cc) = NaN;
+            end
+        end
+    end
+    plot(dsRng,gsRng,'-')
+    legend('s','LSL','RSR','RSL','LSR','RLRo','LRLo','RLRi','LRLi')
+    plot([0 2*dmax],[0 0],'k:')
+    figure(f)
+end
+
+return
+
+G0 = gT;
 % check if we're OK or at a discontinuity
 if abs(G0)<1e-6,
     
@@ -79,7 +217,7 @@ end
 
 end
 
-function G = targetTimeDiff(d,cInit,cTerm,Rmin,vWind,vFly,clMask)
+function [G,pxa,pya,pt,cl] = targetTimeDiff(d,cInit,cTerm,Rmin,vWind,vFly,clMask)
 
 % time for the wind to shift by d units
 tShift = d/norm(vWind);
@@ -88,17 +226,12 @@ tShift = d/norm(vWind);
 cTermShifted = cTerm - [vWind(1);vWind(2);0]*tShift;
 
 % find closest-timed Dubins path of selected class
-[pxa,pya,pt,clInc]=shortestDubinsPath(cInit,cTermShifted,Rmin,clMask);
+[pxa,pya,pt,cl]=shortestDubinsPath(cInit,cTermShifted,Rmin,clMask);
 
 % flight time for real aircraft
 tFly = max(pt)/vFly;
 
 % response is the difference
 G = tFly - tShift;
-
-% trap case if empty
-if isempty(G),
-    G = 1000;
-end
 
 end
--- testWindPath.m
+++ testWindPath.m
@@ -17,21 +17,6 @@ cInit = [0;0;pi/2];
 cTerm = [-1.5;-2;0];
 vWind = [-0.5 0];
 
-% example 2 from paper
-cInit = [0;0;pi/4];
-cTerm = [5;1;pi];
-vWind = [0.5 0];
-
-% % example 3 from paper
-cInit = [0;0;0];
-cTerm = [-6;2;0];
-vWind = [-0.9 0];
-
-% % another one from paper
-cInit = [0;0;0];
-cTerm = [0.005;0.56;0];
-vWind = [-0.9 0];
-
 % option to treat third element as a ground track rather than a heading
 %[cInit(3),Vg]=hdgSpdForTrkInWind(vAir,cInit(3),vWind);
 %[cTerm(3),Vg]=hdgSpdForTrkInWind(vAir,cTerm(3),vWind);
@@ -39,6 +24,7 @@ vWind = [-0.9 0];
 % find the shortest path in wind
 [px,py,pt,pxa,pya,clInc]=shortestWindPath(cInit,cTerm,Rmin,vAir,vWind);
 
+subplot 221
 plot(px,py,'b-', ...
      pxa,pya,'r--', ...
      cInit(1)+[0 cos(cInit(3))],cInit(2)+[0 sin(cInit(3))],'g-',...
@@ -46,3 +32,55 @@ plot(px,py,'b-', ...
 title(sprintf('Path length %5.2f of type %s',max(pt),clInc))
 axis equal
 grid on
+
+% example 2 from paper
+cInit = [0;0;pi/4];
+cTerm = [5;1;pi];
+vWind = [0.5 0];
+
+% find the shortest path in wind
+[px,py,pt,pxa,pya,clInc]=shortestWindPath(cInit,cTerm,Rmin,vAir,vWind);
+
+subplot 222
+plot(px,py,'b-', ...
+     pxa,pya,'r--', ...
+     cInit(1)+[0 cos(cInit(3))],cInit(2)+[0 sin(cInit(3))],'g-',...
+     cTerm(1)+[0 cos(cTerm(3))],cTerm(2)+[0 sin(cTerm(3))],'g-')
+title(sprintf('Path length %5.2f of type %s',max(pt),clInc))
+axis equal
+grid on
+
+% % example 3 from paper
+cInit = [0;0;0];
+cTerm = [-5.5;2;0];
+vWind = [-0.9 0];
+
+% find the shortest path in wind
+[px,py,pt,pxa,pya,clInc]=shortestWindPath(cInit,cTerm,Rmin,vAir,vWind);
+
+subplot 223
+plot(px,py,'b-', ...
+     pxa,pya,'r--', ...
+     cInit(1)+[0 cos(cInit(3))],cInit(2)+[0 sin(cInit(3))],'g-',...
+     cTerm(1)+[0 cos(cTerm(3))],cTerm(2)+[0 sin(cTerm(3))],'g-')
+title(sprintf('Path length %5.2f of type %s',max(pt),clInc))
+axis equal
+grid on
+
+% % another one from paper
+cInit = [0;0;0];
+cTerm = [-0.43;0.56;0];
+vWind = [-0.9 0];
+
+% find the shortest path in wind
+[px,py,pt,pxa,pya,clInc]=shortestWindPath(cInit,cTerm,Rmin,vAir,vWind);
+
+subplot 224
+plot(px,py,'b-', ...
+     pxa,pya,'r--', ...
+     cInit(1)+[0 cos(cInit(3))],cInit(2)+[0 sin(cInit(3))],'g-',...
+     cTerm(1)+[0 cos(cTerm(3))],cTerm(2)+[0 sin(cTerm(3))],'g-')
+title(sprintf('Path length %5.2f of type %s',max(pt),clInc))
+axis equal
+grid on
+
