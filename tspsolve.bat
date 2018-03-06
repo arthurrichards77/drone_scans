36fbb6c781371b96829c59aba9f331597cbd30ef
unknown <aeagr@IT051727.users.bris.ac.uk>
2018-03-06 15:15:26 +0000

    Fixed bug in batch file for Windows TSP solve
    Signed-off-by: unknown <aeagr@IT051727.users.bris.ac.uk>

--- tspsolve.bat
+++ tspsolve.bat
@@ -1,3 +1,4 @@
-set PATH=%PATH;C:\cygwin64\bin
+set PATH=%PATH%;C:\cygwin64\bin
+echo %PATH%
 glpsol -m tsp\tsp.mod -d tsp\tsp.dat -o tsp\tsp.txt
 grep -o 'X\[.*\]\W*\*\W*[01]' tsp\tsp.txt | grep -o '[01]$' > tsp\res.dat
